591e87a0152c51a32699668f8c8d2e0e0fa38562
johnniembugua <johnniendungu321@gmail.com>
2022-05-28 10:35:37 +0300

    Added signin functionality

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,7 @@
-const e = require("express");
+
 const express =require("express");
 const bcryptjs=require("bcryptjs");
+const jwt=require("jsonwebtoken");
 
 const User=require("../models/user");
 
@@ -32,5 +33,28 @@ const hashedPassword= await bcryptjs.hash(password,8);
     }
    
 });
+
+//Signin route
+
+authRouter.post('/api/signin',async(req,res)=>{
+    try {
+        const {email,password}=req.body;
+        const user=await User.findOne({email});
+        if(!user){
+            return res.status(400).json({msg:"User with this email address does not exist"});
+
+        }
+        const isMatch=await bcryptjs.compare(password,user.password);
+        if(!isMatch){
+            return res.status(400).json({msg:"Incorrect email/password"});
+        }
+        const token=jwt.sign({id:user._id},"passwordKey");
+        res.json({token,...user._doc});
+
+
+    } catch (e) {
+        res.status(500).json({error: e.message});
+    }
+})
 
 module.exports=authRouter;
